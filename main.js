28f7cb8ba0a58dd069cd71071c663feacec63bd6
Nicolas Olenev <cardinal2113@gmail.com>
2021-11-04 11:28:03 +0300

    Убраны лишние проверки

--- main.js
+++ main.js
@@ -1,21 +1,16 @@
 const calc = (operation, a, b) => {
-  const checkValue = num => typeof num !== "number" || !isFinite(num);
-  const isIncorrectValue = checkValue(a) || checkValue(b);
-  const checkDivisionByZero = (operation === "div" || operation === "rem") && b === 0;
   const operations = {
-    sum: (a, b) => a + b,
+    sum: (a, b) => +a + +b,
     sub: (a, b) => a - b,
     mult: (a, b) => a * b,
     div: (a, b) => a / b,
   };
 
-  if (isIncorrectValue || checkDivisionByZero) {
-    return "Error";
-  }
+  if (!(operation in operations)) return 'unknown operation';
 
-  if (!(operation in operations)) {
-    return 'unknown operation'
-  }
+  const answer = operations[operation](a, b);
 
-  return operations[operation](a, b);
+  if (!isFinite(answer)) return "Error";
+
+  return answer;
 };
