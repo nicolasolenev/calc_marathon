8c8217a4ec58ac186092c326d555a876e77f4afb
Nicolas Olenev <cardinal2113@gmail.com>
2021-11-04 14:31:16 +0300

    Вернул функции в объект

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 const calc = (operation, a, b) => {
   const operations = {
-    sum: +a + +b,
-    sub: a - b,
-    mult: a * b,
-    div: a / b,
-  }
+    sum: (c, d) => +c + +d,
+    sub: (c, d) => c - d,
+    mult: (c, d) => c * d,
+    div: (c, d) => c / d
+  };
+  console.log(operations);
 
   if (!(operation in operations)) return "unknown operation"; // Если операция не существует
 
-  const answer = operations[operation]; // Произвести вычисление
+  const answer = operations[operation](a, b); // Произвести вычисление
 
   if (!isFinite(answer)) return "Error"; // Если ответ не является числом
 
   return answer;
-}
+};
